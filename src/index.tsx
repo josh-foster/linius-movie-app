67c8e2e4dfb750aea2a82312d37f8b44152ec18d
josh-foster <joshfoster321@hotmail.com>
2021-02-07 13:58:13 +0000

    font fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "typeface-bebas-neue";
 
 ReactDOM.render(
   <React.StrictMode>
